dcf59872eeddb3df02471ec603fe4b0b724aab64
asaki daisuke <mandm6523@gmail.com>
2018-02-17 06:06:05 +0000

    page link add

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,17 +1,18 @@
 class MainController < ApplicationController
   def index
   end
-   def mypage_student
-   end
-   def logout
-   end
-   def student_profile_edit
-   end  
-   def teacher_profile_edit
-   end
-   def teacher_account_edit
-   end
-   def student_account_edit
-   end
-   def teacher_faq
-   end
+  def mypage_student
+  end
+  def logout
+  end
+  def student_profile_edit
+  end  
+  def teacher_profile_edit
+  end
+  def teacher_account_edit
+  end
+  def student_account_edit
+  end
+  def teacher_faq
+  end
+ end
