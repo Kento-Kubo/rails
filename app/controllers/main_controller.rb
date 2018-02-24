1e2ce9938594da2f166a3d60ad9a442d5b18bdc4
asaki daisuke <mandm6523@gmail.com>
2018-02-24 06:49:34 +0000

    modify student_account_edit

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,62 +1,56 @@
 class MainController < ApplicationController
-  def index
-<<<<<<< HEAD
-  end
-   def mypage_student
-   end
-   def logout
-   end
-   def student_profile_edit
-
-=======
->>>>>>> 9398f27f780384afd9537bcd944beaf6036516f2
-   end  
-   def student_profile_edit_confirm
-
-   end
-   def teacher_profile_edit
-   end
-   def teacher_profile_edit_confirm
-   end
-   def teacher_account_edit
-   end
-   def teacher_account_edit_confirm
-   end
-   def student_account_edit
-   end
-   def student_account_edit_confirm
-   end
-   def teacher_faq
-   end
-
-   def student_faq
-   end
-    def student_write_review
-   end   
-   def student_write_review_confirm
-   end   
-
-<<<<<<< HEAD
-end
-=======
-
-  def mypage_student
-  end
-  def logout
-  end
-  def student_profile_edit
-  end  
-  def teacher_profile_edit
-  end
-  def teacher_account_edit
-    @user=find_by[id: params[:id]]
-  end
-  def student_account_edit
-  end
-  def teacher_faq
-  end
-  def student_faq
-  end
- end
->>>>>>> 9398f27f780384afd9537bcd944beaf6036516f2
-
+      def index
+      end
+       def mypage_student
+       end
+       def logout
+       end
+       def student_profile_edit
+    
+       end  
+       def student_profile_edit_confirm
+    
+       end
+       def teacher_profile_edit
+       end
+       def teacher_profile_edit_confirm
+       end
+       def teacher_account_edit
+       end
+       def teacher_account_edit_confirm
+       end
+       def student_account_edit
+       end
+       def student_account_edit_confirm
+       end
+       def teacher_faq
+       end
+    
+       def student_faq
+       end
+        def student_write_review
+       end   
+       def student_write_review_confirm
+       end   
+    
+    
+      def mypage_student
+      end
+      def logout
+      end
+      def student_profile_edit
+      end  
+      def teacher_profile_edit
+      end
+      def teacher_account_edit
+        @user=find_by[id: params[:id]]
+      end
+      def student_account_edit
+      end
+      def teacher_faq
+      end
+      def student_faq
+      end
+     end
+    
+    
