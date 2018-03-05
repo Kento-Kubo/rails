d9fd248356492e53aa25ba30940afca04d92ac58
shervyna ruan <hsuanwei.ruan@yale.edu>
2018-03-05 09:15:08 +0000

    store hashed password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,13 @@
 class User < ActiveRecord::Base
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+    before_save { self.email = email.downcase }
 
-    validates :name, {presence: true ,uniqueness:true}
+    validates :name, {presence: true ,uniqueness:true, length: { maximum: 50 }}
     validates :skype, {presence: true }
-    validates :emails, {presence: true ,uniqueness:true, format: { with: VALID_EMAIL_REGEX }}
+    validates :emails, {presence: true, format: { with: VALID_EMAIL_REGEX },length: { maximum: 255},uniqueness: { case_sensitive: false }}
     validates :password, {presence: true }
-    validates_confirmation_of :password
+    # validates_confirmation_of :password
+  
+    has_secure_password
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180217133935) do
+ActiveRecord::Schema.define(version: 20180302071026) do
 
   create_table "teachers", force: :cascade do |t|
     t.text     "name"
@@ -22,8 +22,8 @@ ActiveRecord::Schema.define(version: 20180217133935) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "emails"
-    t.datetime "created_at",     null: false
-    t.datetime "updated_at",     null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
     t.string   "country"
     t.string   "sex"
     t.string   "skype"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20180217133935) do
     t.integer  "birthday_year"
     t.integer  "birthday_month"
     t.integer  "birthday_day"
+    t.string   "password_digest"
   end
 
 end
