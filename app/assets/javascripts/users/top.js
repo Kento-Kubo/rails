db48acce742438fa66ab4668e8ab508e099d3f21
kento kubo <kento819b@gmail.com>
2018-03-16 15:30:55 +0000

    a

--- app/assets/javascripts/users/top.js
+++ app/assets/javascripts/users/top.js
@@ -1,5 +1,5 @@
 $(function() {　//←消さないこと
-  
+
 // //ログインモーダル表示コード開始---------------------------------------------------------
 
 //   $('#login-show').click(function(){
@@ -24,28 +24,66 @@ $(function() {　//←消さないこと
 // 	*/
 //ログインモーダル表示コード終わり---------------------------------------------------------
 
-//サインアップモーダル表示コード開始---------------------------------------------------------
+//生徒用サインアップモーダル表示コード開始---------------------------------------------------------
 
-  $('#signup-show').click(function(){
-    $('#signup-model').fadeIn();
-  })
+ // $('#signup-show-students').click(function(){
+    
+  //  $('#signup-model-students').fadeIn();
+  //})
   	  
-  $('.close-model').click(function(){
-    $('#signup-model').fadeOut();
-  })
-  /*	  
-  if($('#signup-model').is(':hidden')){
+  //$('.close-model').click(function(){
+  //  $('#signup-model-students').fadeOut();
+  //})
+  
+  
+  
+   //テキストリンクをクリックしたら
+ $("#signup-show-students").click(function(){
+      //body内の最後に<div id="modal-bg"></div>を挿入
+    //$("body").append('<div id="model-bg"></div>');
+ 
+ 
+    //モーダルウィンドウを表示
+      
+        $("#model").fadeIn("slow");
+    //画面のどこかをクリックしたらモーダルを閉じる
+      $("#model-bg").click(function(){
+          $("#close-model").fadeOut("slow",function(){
+         //挿入した<div id="modal-bg"></div>を削除
+            //  $('#model-bg').remove() ;
+         });
+ 
+        });
+ 
+    //画面の左上からmodal-mainの横幅・高さを引き、その値を2で割ると画面中央の位置が計算できます
+     $(window).resize(modalResize);
+      function modalResize(){
+ 
+            var w = $(window).width();
+          var h = $(window).height();
+ 
+            var cw = $("#signup-model-students").outerWidth();
+           var ch = $("#signup-model-students").outerHeight();
+ 
+        //取得した値をcssに追加する
+            $("#signup-model-students").css({
+                "left": ((w - cw)/2) + "px",
+                "top": ((h - ch)/2) + "px"
+          });
+     }
+   });
 
-  }else{
-    $(document).on('click', function(e) {
-    	if(!$(e.target).closest('#signup-model').length && !$(e.target).closest('#signup-show').length){
-        $('#signup-model').fadeOut();
-    	}else{
-        $('#signup-model').fadeOut();
-      }
-		})
-	}
-	*/
-//サインアップモーダル表示コード終わり---------------------------------------------------------
+//生徒用サインアップモーダル表示コード終わり---------------------------------------------------------
+//先生用サインアップモーダル表示コード開始---------------------------------------------------------
+
+//  $('#signup-show-teachers').click(function(){
+//    $('#signup-model-teachers').fadeIn();
+ // })
+  	  
+ // $('.close-model').click(function(){
+ //   $('#signup-model-teachers').fadeOut();
+ // })
+  
+//先生用サインアップモーダル表示コード終わり---------------------------------------------------------
 
 });//←消さないこと
--- db/schema.rb
+++ db/schema.rb
@@ -11,19 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180315122032) do
-
-  create_table "students", force: :cascade do |t|
-    t.text     "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "teachers", force: :cascade do |t|
-    t.text     "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ActiveRecord::Schema.define(version: 20180307102522) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
