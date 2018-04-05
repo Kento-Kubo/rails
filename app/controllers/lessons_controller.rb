0cefd23b18ea6ee78a1357e006e4db4f1a7b56a3
kento kubo <kento819b@gmail.com>
2018-04-05 13:13:59 +0000

    lesson new, create

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,6 @@
 class LessonsController < ApplicationController
- 
+ before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
 
   def index
   end
@@ -12,10 +13,22 @@ class LessonsController < ApplicationController
   end
   
   def create  #make a new reservation
-
-    @lesson = Lesson.new(time: params[:time])
+    @lesson = Lesson.new(time: params[:time],
+                         user_id: current_user.id,
+                         teacher_id: params[:teacher_id],
+                         date: params[:time],
+                         condition: "reserved")
+    @lesson.teacher_id = params[:teacher_id]                 
     @lesson.save
-   
+    
+    if @lesson.save
+      
+      redirect_to("/main/mypage_student")
+    else
+      @error_message = "登録できませんでした。全ての項目を入力されていることを確認してください。"
+      render 'lessons/new'
+    end
+    
   end
   
   def update
